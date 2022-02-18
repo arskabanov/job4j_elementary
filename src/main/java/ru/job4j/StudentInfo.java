6c4c141de37d555ade42d39ed6d60dec8aad456f
arskabanov <mailto:gedeon50@yandex.ru>
2022-02-18 22:06:12 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Arseniy Kabanov");
+        System.out.println("26.04.1983");
+    }
+}
